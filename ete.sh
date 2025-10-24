9bb619490c590f714b2fcd7c3f682a5aa59edd84
Ricardo Samper <ricardosamper@gmail.com>
2025-10-24 15:47:12 -0600

    ti

--- ete.sh
+++ ete.sh
@@ -1,2 +1,2 @@
-clear
+yasdf
 echo "teee"
---
+++ github.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+
+#bash github.sh <cuenta> <repo> <action (pull/push)> 
+
+#ricardosamper
+#https://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@github.com/rsamper/fred.git
+GIT_TOKEN_SAMPER="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"
+GIT_ACCOUNT_SAMPER="rsamper"
+GIT_CONNECTION_SAMPER="https://$GIT_TOKEN_NIC@gihub.com/$GIT_ACCOUNT_NIC/$GIT_REPO_NIC.git"
+GIT_CONNECTION_SAMPER="https://$GIT_TOKEN_NIC@gihub.com/$GIT_ACCOUNT_NIC/$GIT_REPO_NIC.git"
+
+#rsamper
+#https://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@github.com/NIC-sistemas/infraestructura.git
+GIT_TOKEN_NIC="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"
+GIT_ACCOUNT_NIC="NIC-sistemas"
+GIT_CONNECTION_NIC="https://$GIT_TOKEN_NIC@gihub.com/$GIT_ACCOUNT_NIC/$GIT_REPO_NIC.git"
+
+
+###
+GIT_REPO=""
+
+
+# Verificar que se hayan pasado exactamente 2 argumentos
+if [ "$#" -ne 4 ]; then
+    echo "❌ ERROR: Debes proporcionar exactamente tres argumentos."
+    echo "Uso: bash github.sh <cuenta> <repo> <action (pull/push)> "
+    exit 1
+fi
+
+# Si llegamos aquí, tenemos 2 argumentos y podemos usarlos
+account="$1"
+repo="$2"
+git_action="$3"
+commit_message="$4"
+
+
+#echo "✅ OK: Argumentos validados correctamente."
+#echo "Argumento 1 cuenta: $account"
+#echo "Argumento 2 repo: $repo"
+#echo "Argumento 3 action: $git_action"
+#echo "Argumento 4 commit: $commit_message"
+
+if [ "$1" == "nic" ]; then
+    echo "✅ OK: El primer argumento es igual a NIC."
+    git_token=$GIT_TOKEN_NIC
+    git_account="rsamper"
+    git_connection="https://$GIT_TOKEN_NIC@github.com/$GIT_ACCOUNT_NIC/$repo.git"  
+    # Aquí iría el código para ejecutar la lógica de 'db'
+fi
+
+if [ "$1" == "rsamper" ]; then
+    echo "✅ OK: El primer argumento es igual a rsamper."
+    git_token=$GIT_TOKEN_SAMPER
+    git_account="rsamper"
+    git_connection="https://$GIT_TOKEN_SAMPER@github.com/$GIT_ACCOUNT_SAMPER/$repo.git"  
+    # Aquí iría el código para ejecutar la lógica de 'db'
+fi
+
+if [ "$3" == "clone" ]; then
+    echo "✅ OK: El primer argumento es igual a clone."
+    echo "git $git_action $git_connection"
+    git $git_action $git_connection
+    exit 0
+fi
+
+
+if [ "$3" == "pull" ]; then
+    echo "✅ OK: El primer argumento es igual a pull."
+    echo "git $git_action $git_connection"
+    git pull $git_connection
+    exit 0
+fi
+
+if [ "$3" == "push" ]; then
+    echo "✅ OK: El primer argumento es igual a push."
+    git add .
+    git commit -m $4
+    git push  $git_connection
+    exit 0
+fi
+
+exit 0

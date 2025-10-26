51fc552c52215eeed465f6b23b2b25e30e1b4e13
samper <ricardosamper@gmail.com>
2025-10-26 17:33:36 -0600

    a

--- github.sh
+++ github.sh
@@ -1,27 +1,37 @@
 #!/bin/bash
-
-#bash github.sh <cuenta> <repo> <action (pull/push)> 
-
-#ricardosamper
-#https://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@github.com/rsamper/fred.git
-GIT_TOKEN_SAMPER="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"
-GIT_ACCOUNT_SAMPER="rsamper"
-GIT_CONNECTION_SAMPER="https://$GIT_TOKEN_NIC@gihub.com/$GIT_ACCOUNT_NIC/$GIT_REPO_NIC.git"
-GIT_CONNECTION_SAMPER="https://$GIT_TOKEN_NIC@gihub.com/$GIT_ACCOUNT_NIC/$GIT_REPO_NIC.git"
-
-#rsamper
-#https://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@github.com/NIC-sistemas/infraestructura.git
-GIT_TOKEN_NIC="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"
-GIT_ACCOUNT_NIC="NIC-sistemas"
-GIT_CONNECTION_NIC="https://$GIT_TOKEN_NIC@gihub.com/$GIT_ACCOUNT_NIC/$GIT_REPO_NIC.git"
+# Desarrollado por Ricado Samper
+# Uso bash github.sh <cuenta> <repo> <action (pull/push)> 
 
 
-###
-GIT_REPO=""
+#Verifica la existencia de las varialbes de entorno necesarios en archivo ~./bashrc
+#export EMAIL
+#export NAME
+#export GIT_TOKEN_RSAMPER
+#export GIT_TOKEN_NIC
+
+if [[ ! -v EMAIL ]]; then
+    echo "Fallo: EMAIL no existe."
+    exit 1
+fi
+
+if [[ ! -v NAME ]]; then
+    echo "Fallo: NAME no existe."
+    exit 1
+fi
+
+if [[ ! -v GIT_TOKEN_PERSONAL ]]; then
+    echo "Fallo: TOKEN RSAMPER no existe."
+    exit 1
+fi
+
+if [[ ! -v GIT_TOKEN_JOB ]]; then
+    echo "Fallo: TOKEN NIC no existe."
+    exit 1
+fi
 
 
 # Verificar que se hayan pasado exactamente 2 argumentos
-if [ "$#" -ne 4 ]; then
+if [ "$#" -ne 3 ]; then
     echo "❌ ERROR: Debes proporcionar exactamente tres argumentos."
     echo "Uso: bash github.sh <cuenta> <repo> <action (pull/push)> "
     exit 1
@@ -31,28 +41,19 @@ fi
 account="$1"
 repo="$2"
 git_action="$3"
-commit_message="$4"
+git_connection=""
 
-
-
-if [ "$1" == "nic" ]; then
-    #echo "✅ OK: El primer argumento es igual a NIC."
-    git_token=$GIT_TOKEN_NIC
-    git_account="rsamper"
-    git_connection="https://$GIT_TOKEN_NIC@github.com/$GIT_ACCOUNT_NIC/$repo.git"  
-    # Aquí iría el código para ejecutar la lógica de 'db'
+if [ "$1" == "rsamper" ]; then    
+   git_connection="https://$GIT_TOKEN_PERSONAL@github.com/$account/$repo.git"  
+else
+   git_connection="https://$GIT_TOKEN_JOB@github.com/$account/$repo.git"  
 fi
 
-if [ "$1" == "rsamper" ]; then
-    git_token=$GIT_TOKEN_SAMPER
-    git_account="rsamper"
-    git_connection="https://$GIT_TOKEN_SAMPER@github.com/$GIT_ACCOUNT_SAMPER/$repo.git"  
-    # Aquí iría el código para ejecutar la lógica de 'db'
-fi
+
 
 if [ "$3" == "clone" ]; then
-    git "$git_action $git_connection"
-    git $git_action $git_connection
+    echo "git $git_action $git_connection"
+    git $git_action $git_connection > /dev/null
     echo "✅ OK: Operacion exitosa -> clone"
     exit 0
 fi
@@ -61,16 +62,14 @@ if [ "$3" == "pull" ]; then
     echo "git $git_action $git_connection"
     git pull $git_connection > /dev/null
     echo "✅ OK: Operacion exitosa -> pull"
-    exit 0
 fi
 
 if [ "$3" == "push" ]; then
     echo "git push $git_connection"
-    git add . > /dev/null
-    git commit -m $4 > /dev/null
+    #3git add . > /dev/null
+    #git commit -m $4 > /dev/null
     git push  $git_connection > /dev/null
     echo "✅ OK: Operacion exitosa -> push"
-    exit 0
 fi
 
 exit 0

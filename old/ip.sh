f968c9f8864f3a5d800518bbc601419d84c45f0a
samper <ricardosamper@gmail.com>
2026-01-08 10:05:08 -0600

    remoded fred old

--- old/ip.sh
+++
@@ -1,7 +0,0 @@
-
-# ESTO FUNCIONA Y ES LA FORMA RECOMENDADA
-SCRIPT_DIR="$(dirname "${BASH_SOURCE[0]}")"
-
-
-mapfile -t packages < <(grep -v '^#' "$SCRIPT_DIR/pkg.base" | grep -v '^$')
-echo ${packages[@]}

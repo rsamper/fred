67236bf53566fbcab4a4e2f5ae896ac17b3ebac5
samper <ricardosamper@gmail.com>
2026-01-22 17:12:00 -0600

    Hasta add credit

--- init_fred.sh
+++ init_fred.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
+HANDLE="REG-SYSTEM"
+ZONE_FQDN="cr"
 
 pause(){
   sleep 1
   # Pausa hasta que el usuario presione una tecla
+  echo
   read -n 1 -s -r -p "Presiona cualquier tecla para continuar..."
   echo
   #echo "Continuando la ejecución..."
@@ -52,13 +55,13 @@ exec_command() {
 
   # Validar que se recibió al menos un elemento
   if [ ${#comando_a_ejecutar[@]} -eq 0 ]; then
-    echo "❌ Error: No se recibió ningún comando para ejecutar."
+    echo "Error: No se recibió ningún comando para ejecutar."
     return 1
   fi
 
   # Mostrar el comando que se ejecutará
   echo "Ejecutando..."
-  #printf ' %q' "${comando_a_ejecutar[@]}"
+  printf ' %q' "${comando_a_ejecutar[@]}"
   #echo
 
   #echo "Iniciando ejecución..."
@@ -70,45 +73,49 @@ exec_command() {
 
   # Verificar resultado
   if [ $status -eq 0 ]; then
+    echo ""
     echo "Éxito: El comando se completó correctamente."
     return 0
   else
+    echo ""
     echo "Error: El comando falló durante la ejecución."
     echo "$output"
+    #exit 1 
     return 1
   fi
 }
 
 add_credit() {
-  clear
+  
   echo "================================="
   echo "7/8  Agregar credito a un registrador"
   echo "================================="
   REG_ID=$(ask "Registrar ID") || return 1
+  ZONE_ID=$(ask "ZONE ID") || return 1
   CREDIT=$(ask "Credito ") || return
   
-  cmd=(fred-admin --invoice_credit --zone_id=1 --registrar_id=$REG_ID --price=$CREDIT)
+  cmd=(fred-admin --invoice_credit --zone_id="$ZONE_ID" --registrar_id="$REG_ID" --price="$CREDIT")
   exec_command "${cmd[@]}"
 }
 
 
 add_invoice_number() {
-  clear
+  
   echo "================================="
   echo "7/8  Crear prefijo de facturacion"
   echo "================================="
 
   PREFIX=$(ask "Prefix for advance") || return 1
-  cmd=(fred-admin --add_invoice_number_prefix --prefix=$PREFIX --zone_fqdn=$ZONE_FQDN --invoice_type_name=advance)
+  cmd=(fred-admin --add_invoice_number_prefix --prefix="$PREFIX" --zone_fqdn="$ZONE_FQDN" --invoice_type_name=advance)
   exec_command "${cmd[@]}"
   PREFIX=$(ask "Prefix for account") || return 1
-  cmd=(fred-admin --add_invoice_number_prefix --prefix=$PREFIX --zone_fqdn=cz --invoice_type_name=account)
+  cmd=(fred-admin --add_invoice_number_prefix --prefix="$PREFIX" --zone_fqdn="$ZONE_FQDN" --invoice_type_name=account)
   exec_command "${cmd[@]}"
 }
 
 
 add_price_general() {
-  clear
+  
   echo "================================="
   echo "6/8  Crear la lista de precios del sistema.  Precio operacion EPP General"
   echo "================================="
@@ -116,25 +123,25 @@ add_price_general() {
 
   VALID_FROM=$(ask "Valid from (ej 2014-12-31 23:00:00)") || return
   PRICE=$(ask "Precio por operacion EPP (ej 25)") || return
-  cmd=(fred-admin -fred-admin --price_add --operation='RenewDomain' --zone_fqdn=$ZONE_FQDN --valid_from=$VALID_FROM --operation_price $PRICE --period 1)
+  cmd=(fred-admin --price_add --operation="'GeneralEppOperation'" --zone_fqdn="$ZONE_FQDN" --valid_from="$VALID_FROM" --operation_price "$PRICE" --period 1)
+
   exec_command "${cmd[@]}"
 }
 
 add_price_renew() {
 
-  clear
+  
   echo "================================="
   echo "6/8  Crear la lista de precios del sistema".  Renovacion de dominios
   echo "================================="
 
   VALID_FROM=$(ask "Valid from (ej 2014-12-31 23:00:00)") || return
   PRICE=$(ask "Precio de renovacion de dominios (ej 25)") || return
-  cmd=(fred-admin -fred-admin --price_add --operation='RenewDomain' --zone_fqdn=$ZONE_FQDN --valid_from=$VALID_FROM --operation_price $PRICE --period 1)
+  cmd=(fred-admin --price_add --operation="'RenewDomain'" --zone_fqdn="$ZONE_FQDN" --valid_from="$VALID_FROM" --operation_price "$PRICE" --period 1)
   exec_command "${cmd[@]}"
 }
 
 add_price_create() {
-  clear
   echo "================================="
   echo "6/8  Crear la lista de precios del sistema.  Creacion de Dominios"
   echo "================================="
@@ -144,25 +151,26 @@ add_price_create() {
   VALID_FROM=$(ask "Valid from ej 2014-12-31 23:00:00") || return 1
   PRICE=$(ask "Precio por crear dominios (ej 25)") || return 1
   
-  cmd=(fred-admin --price_add --operation='CreateDomain' --zone_fqdn=$ZONE_FQDN --valid_from=$VALID_FROM --operation_price $PRICE --period 1)
+  cmd=(fred-admin --price_add --operation="'CreateDomain'" --zone_fqdn="$ZONE_FQDN" --valid_from="$VALID_FROM" --operation_price "$PRICE" --period 1)
+      #fred-admin --price_add --operation='CreateDomain' --zone_fqdn=cr --valid_from='2014-12-31 23:00:00'  --operation_price 333 --period 1
   exec_command "${cmd[@]}"
 }
 
 grant_zone_access() {
-  clear
+  
   echo "================================="
   echo "5/8  Dar acceso a la zona al Registrador"
   echo "================================="
   
   FROM_DATE=$(ask "From date (YYYY-MM-DD)") || return
 
-  cmd=(fred-admin --registrar_add_zone --zone_fqdn="$ZONE_FQDN" --handle="$REG_HANDLE" --from_date="$FROM_DATE")
+  cmd=(fred-admin --registrar_add_zone --zone_fqdn="$ZONE_FQDN" --handle="$HANDLE" --from_date="$FROM_DATE")
   exec_command "${cmd[@]}"  
 }
 
 
 add_registrar_acl() {
-  clear
+  
   echo "================================="
   echo "4/8  Dar acceso al Registrador del Sistema"
   echo "================================="
@@ -175,7 +183,8 @@ add_registrar_acl() {
   fi
   FINGERPRINT=$(openssl x509 -noout -fingerprint -md5 -in /usr/share/fred-client/ssl/test-cert.pem | cut -d= -f2)
 
-
+  #echo $FINGERPRINT
+  #exit 1  
   if [[ -z "$FINGERPRINT" ]]; then
     echo "❌ No se pudo obtener fingerprint del certificado"
     return 1 
@@ -185,48 +194,64 @@ add_registrar_acl() {
 
   PASS=$(ask "Ingrese la clave para el registrador") || return 1
 
-  cmd=(fred-admin --registrar_acl_add  --handle="$REG_HANDLE" --certificate="$CERT" --password="$PASS")
+  cmd=(fred-admin --registrar_acl_add  --handle="$HANDLE" --certificate="$FINGERPRINT" --password="$PASS")
   exec_command "${cmd[@]}"
 }
 
 
 add_registrar() {
-  clear
+  
   echo "================================="
   echo "3/8  Agregar Registrador del Sistema"
   echo "================================="
   
-  NAME=$(ask "Registrar name") || return 1
-  REG_HANDLE=$(ask "HANDLE ") || return 1
-  ORGANIZATION=$(ask "Organization") || return 1
-  COUNTRY=$(ask "Country") || return 1
+  #NAME=$(ask "Registrar name") || return 1
+  HANDLE=$(ask "HANDLE ej REG-SYSTEM") || return 1
+  ORGANIZATION=$(ask "Organization ej NICCR") || return 1
+  COUNTRY=$(ask "Country ej CR") || return 1
+  CITY=$(ask "City ej 27A") || return 1
+  STREET=$(ask "Street ej SJ") || return 1
+  EMAIL=$(ask "Organization Email  ej ti@nic.cr") || return 1
+  URL=$(ask "Organization URL  ej www.nic.cr") || return 1
  
   
-  cmd=(fred-admin --registrar_add --handle=$HANDLE --reg_name="NAME" --country=$COUNTRY --no_vat --system)
+  cmd=(fred-admin --registrar_add --handle=$HANDLE --reg_name=$HANDLE --country=$COUNTRY --organization=$ORGANIZATION --street1=$STREET --city=$CITY --email=$EMAIL --url=$URL --dic=0000 --no_vat --system)
+  # fred-admin --registrar_add 
+  #--handle=REG-SYSTEM 
+  #--reg_name=REG-SYSTEM 
+  #--country=CR    
+  #--organization=NICCR        
+  #--street1=27A     
+  #--city=SJO 
+  #--email=ti@nic.cr 
+  #--url=http://www.nic.cr/ 
+  #--dic=0000 
+  #--no_vat 
+  #--system
   exec_command "${cmd[@]}"
 }
 
 
 add_nameservers() {
-  clear
+  
   echo "================================="
   echo "Paso 2/8.  Agregando NSs a la zona"
   echo "================================="
  
-  while true; do
+  #while true; do
     NS=$(ask "Agregar NS (vacío para terminar)") || return 1
-    [ -z "$NS" ] && break
-    ADDR=$(ask "IP(s) del NS (opcional)") || return 1
+   # [ -z "$NS" ] && break
+    ADDR=$(ask "IP del NS") || return 1
     cmd=(fred-admin --zone_ns_add --zone_fqdn="$ZONE_FQDN" --ns_fqdn="$NS")
-    [ -n "$ADDR" ] && cmd+=(--addr $ADDR)
+   # [ -n "$ADDR" ] && cmd+=(--addr $ADDR)
     exec_command "${cmd[@]}"
-  done
+  #done
 }
 
 form_zone() {
   ZONE_FQDN=$(ask "Ingrese el FQDN de la zona (ej: cr)") || return 1
   TTL=$(ask "Ingrese el TTL (ej: 18000)") || return 1
-  HOSTMASTER=$(ask "Ingrese el email del Hostmaster ") || return 1
+  HOSTMASTER=$(ask "Ingrese el email del Hostmaster ej ti.nic.cr") || return 1
   NS_FQDN=$(ask "Ingrese el nombre del servidor DNS primario (ej: ns.nic.cr)") || return 1
   return 0
 }
@@ -280,13 +305,22 @@ print_info(){
 
 
 print_info
-create_zone 
-add_nameservers
-add_registrar
-add_registrar_acl
-grant_zone_access
-add_price_create
-add_price_renew
-add_price_general
+#create_zone
+#pause 
+#add_nameservers
+#pause
+#add_registrar
+#add_registrar_acl
+#pause
+#grant_zone_access
+#pause
+#add_price_create
+#pause
+#add_price_renew
+#pause
+#add_price_general
+#pause
 add_invoice_number
+pause
 add_credit
+pause

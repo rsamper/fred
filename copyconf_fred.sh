f2987ac91dd3cd55551af244ac6d35f0a1afc6f7
samper <ricardosamper@gmail.com>
2026-01-29 16:14:11 -0600

    fix file name printf fix

--- copyconf_fred.sh
+++ copyconf_fred.sh
@@ -7,23 +7,24 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 
 copy_fred_configs() {
-  print_message "Copiando archivos de configuracion FRED /etc/fred/Copying FRED config files"
+  echo "Copiando archivos de configuracion FRED /etc/fred/Copying FRED config files"
   #rm /etc/fred/*.conf
   rm -rf /etc/fred/*
   cp -av ./configs/* /etc/fred
-  print_message "OK: !!!!!"
+  printf "OK: !!!!!"
 }
 
 
 init(){
-  print_message  "Initialazing..."  
-  print_message  "Init complete: Ok"
+  printf "Initialazing..."  
+  printf "Init complete: Ok"
 }
 
 
 # Main function
 main() {
-   print_message  "Running"
+   clear
+   printf  "Running"
    init
    copy_fred_configs
 }
